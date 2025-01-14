35b44af7e6e2ea8098641d953e95d4fe3a2eafb8
Mahdy <mradmehdi59@gmail.com>
2025-01-14 11:52:11 +0100

    Initial setup with Tailwind CSS

---
+++ src/App2.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+      return {
+        name : 'John',
+        status: 'pending' ,
+      };    
+  },
+}
+</script>
+
+
+<template>
+  <h1 style="color: blue;"> {{ name }}</h1>
+  <p v-if="status"> User active</p>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <header class="bg-blue-600 text-white p-4">
+      <h1 class="text-xl">Customer Rewards Management</h1>
+    </header>
+  </template>
+  
